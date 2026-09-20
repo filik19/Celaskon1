34ae04e7e942b4bb4f0eebd546a814a68a05d0dc
filik19 <kozicka159@gmail.com>
2026-09-20 21:06:49 +0200

    Added crash codes and and redesigned warnbox to hibox.

--- Celaskon 1/Program.cs
+++ Celaskon 1/Program.cs
@@ -19,15 +19,15 @@ class Program
         // unused now
         //# uint flags = 0x00000004 | 0x00000040;
 
-        Console.WriteLine("Thanks for downloading my little program :)");
+        Console.WriteLine("Thanks for downloading my little program  :)");
         Console.WriteLine("");
 
-        int response1 = MessageBox(IntPtr.Zero, "This is NOT a virus, it will NOT damage your fucking computer. Do you want to 'continue = true'?", "Celaskon 1", 0x00000004);
+        int response1 = MessageBox(IntPtr.Zero, "Hello! Do you want to continue?", "Celaskon 1", 0x00000004);
 
 
         // 6 = Yes
         // 7 = No
-        // 2 = cancel i think
+        // 2 = Cancel i think
         switch (response1)
         {
             case 6:
@@ -35,26 +35,33 @@ class Program
                 break;
             case 7:
                 Console.WriteLine("no from response1");
-                break;
-            case 2:
-                Console.WriteLine(" from response1");
+                Console.WriteLine("Quitting with code 1");
+                Environment.Exit(1);
                 break;
         }
 
         MessageBox(IntPtr.Zero, "You started coughing...", "Celaskon 1", 0x00000000 | 0x00000040);
+        Console.WriteLine("comfirmed info1");
 
-        int response2 = MessageBox(IntPtr.Zero, "Do you want to take celaskon?", "Celaskon 1", 0x00000004 | 0x00000040);
+        int response2 = MessageBox(IntPtr.Zero, "Do you want to take celaskon?", "Celaskon 1", 0x00000004 | 0x00000020);
 
         switch (response2)
         {
             case 6:
                 Console.WriteLine("yes from response2");
-                MessageBox(IntPtr.Zero, "You got the celaskon... That means...", "Celaskon 1", 0x00000000 | 0x00000040);
+                MessageBox(IntPtr.Zero, "You got the celaskon... That means...", "Celaskon 1", 0x00000000);
+                Console.WriteLine("comfirmed info2");
                 MessageBox(IntPtr.Zero, "YOU LIVE!!", "Celaskon 1", 0x00000000 | 0x00000040);
+                Console.WriteLine("comfirmed info3");
+                Console.WriteLine("Quitting with code 0");
+                Environment.Exit(0);
                 break;
             case 7:
                 Console.WriteLine("no from response2");
-                MessageBox(IntPtr.Zero, "You died.", "Celaskon 1", 0x00000000 | 0x00000040);
+                MessageBox(IntPtr.Zero, "You died.", "Celaskon 1", 0x00000000 | 0x00000030);
+                Console.WriteLine("comfirmed info4");
+                Console.WriteLine("Quitting with code 0");
+                Environment.Exit(0);
                 break;
         }
 
